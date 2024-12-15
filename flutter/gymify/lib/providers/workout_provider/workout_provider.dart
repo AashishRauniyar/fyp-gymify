8e0a554b72969c144c86b2c60f848292b71ce291
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-12-16 03:00:30 +0545

    added search bar in workout page

--- flutter/gymify/lib/providers/workout_provider/workout_provider.dart
+++ flutter/gymify/lib/providers/workout_provider/workout_provider.dart
@@ -1,3 +1,192 @@
+// import 'dart:io';
+
+// import 'package:dio/dio.dart';
+// import 'package:flutter/material.dart';
+// import 'package:gymify/models/api_response.dart';
+// import 'package:gymify/models/workout_model.dart'; // Import the Workout model.
+// import 'package:gymify/network/http.dart';
+// import 'package:gymify/providers/auth_provider/auth_provider.dart';
+// import 'package:gymify/services/storage_service.dart'; // Make sure the httpClient is properly set up.
+// import 'package:provider/provider.dart';
+
+// class WorkoutProvider with ChangeNotifier {
+//   List<Workout> _workouts = [];
+//   List<Workout> get workouts => _workouts;
+
+//   bool _hasError = false;
+//   bool get hasError => _hasError;
+
+//   Workout? _selectedWorkout;
+//   Workout? get selectedWorkout => _selectedWorkout;
+
+//   final StorageService _storageService = SharedPrefsService();
+
+//   // Fetch all workouts
+//   Future<void> fetchAllWorkouts() async {
+//     try {
+//       final response = await httpClient
+//           .get('/workouts'); // Adjust the endpoint to your actual API
+
+//       final apiResponse = ApiResponse<List<Workout>>.fromJson(
+//         response.data,
+//         (data) => (data as List).map((item) => Workout.fromJson(item)).toList(),
+//       );
+
+//       if (apiResponse.status == 'success') {
+//         _workouts = apiResponse.data;
+//         notifyListeners(); // Notify listeners when data is updated.
+//       } else {
+//         print('Error: ${apiResponse.message}');
+//         throw Exception(apiResponse.message.isNotEmpty
+//             ? apiResponse.message
+//             : 'Unknown error');
+//       }
+//     } catch (e) {
+//       print('Error fetching workouts: $e');
+//       throw Exception('Error fetching workouts: $e');
+//     }
+//   }
+
+// //----------------------------------------------------------------
+
+// //TODO: to handle unique name for workout
+//   // Create a new workout
+//   Future<int> createWorkout({
+//     required BuildContext context,
+//     required String workoutName,
+//     required String description,
+//     required String targetMuscleGroup,
+//     required String difficulty,
+//     required String goalType,
+//     required String fitnessLevel,
+//     File? workoutImage, // Optional: To upload image
+//   }) async {
+//     try {
+//       final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+//       final trainerId =
+//           authProvider.userId; // Assuming user ID is saved as 'user_id'
+
+//       if (trainerId == null) {
+//         throw Exception('Trainer ID not found');
+//       }
+
+//       // Prepare the data for the workout
+//       FormData formData = FormData.fromMap({
+//         'workout_name': workoutName,
+//         'description': description,
+//         'target_muscle_group': targetMuscleGroup,
+//         'difficulty': difficulty,
+//         'goal_type': goalType,
+//         'fitness_level': fitnessLevel,
+//         'trainer_id': trainerId,
+//         // Set the trainer's ID
+//         if (workoutImage != null)
+//           'workout_image': await MultipartFile.fromFile(
+//             workoutImage.path,
+//             filename: 'profile_image.jpeg',
+//             contentType: getContentType(workoutImage),
+//           ),
+//       });
+
+//       final response = await httpClient.post(
+//         '/create-workouts',
+//         options: Options(headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'multipart/form-data'
+//         }),
+//         data: formData,
+//       );
+
+//       final apiResponse = ApiResponse<Map<String, dynamic>>.fromJson(
+//         response.data,
+//         (data) => data as Map<String, dynamic>,
+//       );
+
+//       if (apiResponse.status == 'success') {
+//         // Extract the workout_id
+//         final workoutData = apiResponse.data;
+//         // Extract the workout_id
+//         final workoutId = workoutData['workout_id'] as int;
+//         print(workoutId);
+
+//         // Optionally, fetch all workouts if needed
+//         fetchAllWorkouts();
+
+//         // Return the workout_id
+//         return workoutId;
+//       } else {
+//         throw Exception(apiResponse.message.isNotEmpty
+//             ? apiResponse.message
+//             : 'Unknown error');
+//       }
+//     } catch (e) {
+//       print('Error creating workout: $e');
+//       throw Exception('Error creating workout: $e');
+//     }
+//   }
+
+//   //----------------------------------------------------------------------------
+
+//   Future<void> addExercisesToWorkout(BuildContext context, int workoutId,
+//       List<Map<String, dynamic>> exercises) async {
+//     try {
+//       print('Request payload: ${exercises.toString()}');
+//       final response = await httpClient.post(
+//         '/workouts/$workoutId/exercises',
+//         data: {'exercises': exercises},
+//         options: Options(headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json',
+//         }),
+//       );
+
+//       final responseData = response.data;
+//       print('API Response: $responseData');
+
+//       if (responseData['status'] == 'success') {
+//         final addedExercises = (responseData['data'] as List<dynamic>)
+//             .map((exercise) => exercise as Map<String, dynamic>)
+//             .toList();
+//         print('Added exercises: $addedExercises');
+//         notifyListeners();
+//       } else {
+//         print('API Error: ${responseData['message']}');
+//         throw Exception(responseData['message']);
+//       }
+//     } catch (e) {
+//       print('Error adding exercises to workout: $e');
+//       throw Exception('Error adding exercises: $e');
+//     }
+//   }
+
+//   Future<void> fetchWorkoutById(int workoutId) async {
+//     _hasError = false; // Reset error state
+//     try {
+//       final response = await httpClient.get('/workouts/$workoutId');
+//       final apiResponse = ApiResponse<Workout>.fromJson(
+//         response.data,
+//         (data) => Workout.fromJson(data as Map<String, dynamic>),
+//       );
+
+//       if (apiResponse.status == 'success') {
+//         _selectedWorkout = apiResponse.data;
+//       } else {
+//         _hasError = true;
+//       }
+//     } catch (e) {
+//       _hasError = true;
+//       debugPrint('[WorkoutProvider] Error fetching workout by ID: $e');
+//     }
+//     notifyListeners();
+//   }
+
+//   // Reset the selected workout (optional)
+//   void clearSelectedWorkout() {
+//     _selectedWorkout = null;
+//     notifyListeners();
+//   }
+// }
 import 'dart:io';
 
 import 'package:dio/dio.dart';
@@ -19,13 +208,26 @@ class WorkoutProvider with ChangeNotifier {
   Workout? _selectedWorkout;
   Workout? get selectedWorkout => _selectedWorkout;
 
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
   final StorageService _storageService = SharedPrefsService();
+
+  void _setLoading(bool loading) {
+    _isLoading = loading;
+    notifyListeners();
+  }
+
+  void _setError(bool error) {
+    _hasError = error;
+    notifyListeners();
+  }
 
   // Fetch all workouts
   Future<void> fetchAllWorkouts() async {
+    _setLoading(true); // Start loading
     try {
-      final response = await httpClient
-          .get('/workouts'); // Adjust the endpoint to your actual API
+      final response = await httpClient.get('/workouts');
 
       final apiResponse = ApiResponse<List<Workout>>.fromJson(
         response.data,
@@ -34,22 +236,20 @@ class WorkoutProvider with ChangeNotifier {
 
       if (apiResponse.status == 'success') {
         _workouts = apiResponse.data;
-        notifyListeners(); // Notify listeners when data is updated.
       } else {
-        print('Error: ${apiResponse.message}');
+        _setError(true);
         throw Exception(apiResponse.message.isNotEmpty
             ? apiResponse.message
             : 'Unknown error');
       }
     } catch (e) {
+      _setError(true);
       print('Error fetching workouts: $e');
-      throw Exception('Error fetching workouts: $e');
+    } finally {
+      _setLoading(false); // Stop loading
     }
   }
 
-//----------------------------------------------------------------
-
-//TODO: to handle unique name for workout
   // Create a new workout
   Future<int> createWorkout({
     required BuildContext context,
@@ -61,17 +261,15 @@ class WorkoutProvider with ChangeNotifier {
     required String fitnessLevel,
     File? workoutImage, // Optional: To upload image
   }) async {
+    _setLoading(true); // Start loading
     try {
       final authProvider = Provider.of<AuthProvider>(context, listen: false);
-
-      final trainerId =
-          authProvider.userId; // Assuming user ID is saved as 'user_id'
+      final trainerId = authProvider.userId;
 
       if (trainerId == null) {
         throw Exception('Trainer ID not found');
       }
 
-      // Prepare the data for the workout
       FormData formData = FormData.fromMap({
         'workout_name': workoutName,
         'description': description,
@@ -80,7 +278,6 @@ class WorkoutProvider with ChangeNotifier {
         'goal_type': goalType,
         'fitness_level': fitnessLevel,
         'trainer_id': trainerId,
-        // Set the trainer's ID
         if (workoutImage != null)
           'workout_image': await MultipartFile.fromFile(
             workoutImage.path,
@@ -104,16 +301,8 @@ class WorkoutProvider with ChangeNotifier {
       );
 
       if (apiResponse.status == 'success') {
-        // Extract the workout_id
-        final workoutData = apiResponse.data;
-        // Extract the workout_id
-        final workoutId = workoutData['workout_id'] as int;
-        print(workoutId);
-
-        // Optionally, fetch all workouts if needed
-        fetchAllWorkouts();
-
-        // Return the workout_id
+        final workoutId = apiResponse.data['workout_id'] as int;
+        await fetchAllWorkouts(); // Refresh workouts
         return workoutId;
       } else {
         throw Exception(apiResponse.message.isNotEmpty
@@ -123,15 +312,16 @@ class WorkoutProvider with ChangeNotifier {
     } catch (e) {
       print('Error creating workout: $e');
       throw Exception('Error creating workout: $e');
+    } finally {
+      _setLoading(false); // Stop loading
     }
   }
 
-  //----------------------------------------------------------------------------
-
+  // Add exercises to a workout
   Future<void> addExercisesToWorkout(BuildContext context, int workoutId,
       List<Map<String, dynamic>> exercises) async {
+    _setLoading(true); // Start loading
     try {
-      print('Request payload: ${exercises.toString()}');
       final response = await httpClient.post(
         '/workouts/$workoutId/exercises',
         data: {'exercises': exercises},
@@ -142,26 +332,24 @@ class WorkoutProvider with ChangeNotifier {
       );
 
       final responseData = response.data;
-      print('API Response: $responseData');
 
       if (responseData['status'] == 'success') {
-        final addedExercises = (responseData['data'] as List<dynamic>)
-            .map((exercise) => exercise as Map<String, dynamic>)
-            .toList();
-        print('Added exercises: $addedExercises');
         notifyListeners();
       } else {
-        print('API Error: ${responseData['message']}');
         throw Exception(responseData['message']);
       }
     } catch (e) {
       print('Error adding exercises to workout: $e');
       throw Exception('Error adding exercises: $e');
+    } finally {
+      _setLoading(false); // Stop loading
     }
   }
 
+  // Fetch workout by ID
   Future<void> fetchWorkoutById(int workoutId) async {
-    _hasError = false; // Reset error state
+    _setLoading(true); // Start loading
+    _setError(false); // Reset error state
     try {
       final response = await httpClient.get('/workouts/$workoutId');
       final apiResponse = ApiResponse<Workout>.fromJson(
@@ -172,16 +360,17 @@ class WorkoutProvider with ChangeNotifier {
       if (apiResponse.status == 'success') {
         _selectedWorkout = apiResponse.data;
       } else {
-        _hasError = true;
+        _setError(true);
       }
     } catch (e) {
-      _hasError = true;
-      debugPrint('[WorkoutProvider] Error fetching workout by ID: $e');
+      _setError(true);
+      print('Error fetching workout by ID: $e');
+    } finally {
+      _setLoading(false); // Stop loading
     }
-    notifyListeners();
   }
 
-  // Reset the selected workout (optional)
+  // Reset selected workout
   void clearSelectedWorkout() {
     _selectedWorkout = null;
     notifyListeners();
--- flutter/gymify/lib/screens/main_screens/workout_screens/workout_screen.dart
+++ flutter/gymify/lib/screens/main_screens/workout_screens/workout_screen.dart
@@ -1,10 +1,104 @@
+// import 'package:flutter/material.dart';
+// import 'package:gymify/screens/main_screens/workout_screens/workout_details_screen.dart';
+// import 'package:provider/provider.dart';
+// import 'package:gymify/providers/workout_provider/workout_provider.dart';
+
+// class WorkoutListScreen extends StatelessWidget {
+//   const WorkoutListScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Workouts'),
+//       ),
+//       body: ChangeNotifierProvider(
+//         create: (_) => WorkoutProvider()..fetchAllWorkouts(),
+//         child: Consumer<WorkoutProvider>(
+//           builder: (context, workoutProvider, child) {
+//             if (workoutProvider.workouts.isEmpty) {
+//               return const Center(child: CircularProgressIndicator());
+//             }
+
+//             return ListView.builder(
+//               itemCount: workoutProvider.workouts.length,
+//               itemBuilder: (context, index) {
+//                 final workout = workoutProvider.workouts[index];
+//                 return Card(
+//                   margin:
+//                       const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+//                   elevation: 5,
+//                   shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(12)),
+//                   child: InkWell(
+//                     onTap: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) =>
+//                               WorkoutDetailScreen(workoutId: workout.workoutId),
+//                         ),
+//                       );
+//                     },
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         image: DecorationImage(
+//                           image: workout.workoutImage.isNotEmpty
+//                               ? NetworkImage(workout.workoutImage)
+//                               : const AssetImage(
+//                                       'assets/images/workout_image/defaultWorkoutImage.jpg')
+//                                   as ImageProvider,
+//                           fit: BoxFit.cover,
+//                           colorFilter: ColorFilter.mode(
+//                             Colors.black.withOpacity(0.5),
+//                             BlendMode.darken,
+//                           ),
+//                         ),
+//                         borderRadius: BorderRadius.circular(12),
+//                       ),
+//                       child: ListTile(
+//                         contentPadding: const EdgeInsets.all(16),
+//                         title: Text(
+//                           workout.workoutName,
+//                           style: const TextStyle(
+//                               color: Colors.white,
+//                               fontSize: 22,
+//                               fontWeight: FontWeight.bold),
+//                         ),
+//                         subtitle: Text(
+//                           workout.targetMuscleGroup,
+//                           style: const TextStyle(
+//                               color: Colors.white, fontSize: 16),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 );
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//! with search bar
 import 'package:flutter/material.dart';
 import 'package:gymify/screens/main_screens/workout_screens/workout_details_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:gymify/providers/workout_provider/workout_provider.dart';
 
-class WorkoutListScreen extends StatelessWidget {
+class WorkoutListScreen extends StatefulWidget {
   const WorkoutListScreen({super.key});
+
+  @override
+  _WorkoutListScreenState createState() => _WorkoutListScreenState();
+}
+
+class _WorkoutListScreenState extends State<WorkoutListScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
@@ -16,65 +110,110 @@ class WorkoutListScreen extends StatelessWidget {
         create: (_) => WorkoutProvider()..fetchAllWorkouts(),
         child: Consumer<WorkoutProvider>(
           builder: (context, workoutProvider, child) {
-            if (workoutProvider.workouts.isEmpty) {
+            if (workoutProvider.isLoading) {
               return const Center(child: CircularProgressIndicator());
             }
 
-            return ListView.builder(
-              itemCount: workoutProvider.workouts.length,
-              itemBuilder: (context, index) {
-                final workout = workoutProvider.workouts[index];
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              WorkoutDetailScreen(workoutId: workout.workoutId),
+            if (workoutProvider.workouts.isEmpty) {
+              return const Center(child: Text('No workouts found.'));
+            }
+
+            // Filter workouts based on the search query
+            final filteredWorkouts = workoutProvider.workouts.where((workout) {
+              final workoutName = workout.workoutName.toLowerCase();
+              final targetMuscleGroup = workout.targetMuscleGroup.toLowerCase();
+              return workoutName.contains(_searchQuery) ||
+                  targetMuscleGroup.contains(_searchQuery);
+            }).toList();
+
+            return Column(
+              children: [
+                // Search Bar
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: TextField(
+                    controller: _searchController,
+                    onChanged: (value) {
+                      setState(() {
+                        _searchQuery = value.toLowerCase();
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Search Workouts...',
+                      prefixIcon: const Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                    ),
+                  ),
+                ),
+
+                // Workout List
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredWorkouts.length,
+                    itemBuilder: (context, index) {
+                      final workout = filteredWorkouts[index];
+                      return Card(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 15),
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => WorkoutDetailScreen(
+                                    workoutId: workout.workoutId),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: workout.workoutImage.isNotEmpty
+                                    ? NetworkImage(workout.workoutImage)
+                                    : const AssetImage(
+                                            'assets/images/workout_image/defaultWorkoutImage.jpg')
+                                        as ImageProvider,
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(
+                                  Colors.black.withOpacity(0.5),
+                                  BlendMode.darken,
+                                ),
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.all(16),
+                              title: Text(
+                                workout.workoutName,
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                workout.targetMuscleGroup,
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                            ),
+                          ),
                         ),
                       );
                     },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: workout.workoutImage.isNotEmpty
-                              ? NetworkImage(workout.workoutImage)
-                              : const AssetImage(
-                                      'assets/images/workout_image/defaultWorkoutImage.jpg')
-                                  as ImageProvider,
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(
-                            Colors.black.withOpacity(0.5),
-                            BlendMode.darken,
-                          ),
-                        ),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: ListTile(
-                        contentPadding: const EdgeInsets.all(16),
-                        title: Text(
-                          workout.workoutName,
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          workout.targetMuscleGroup,
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 16),
-                        ),
-                      ),
-                    ),
                   ),
-                );
-              },
+                ),
+              ],
             );
           },
         ),
@@ -82,4 +221,3 @@ class WorkoutListScreen extends StatelessWidget {
     );
   }
 }
-
