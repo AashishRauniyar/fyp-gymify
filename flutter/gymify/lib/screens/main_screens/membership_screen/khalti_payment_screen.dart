f840e8a0cb6ac493a1f6c0cb2e9113f424c6dae8
AashishRauniyar <rauniyaaraashish@gmail.com>
2025-03-01 12:04:35 +0545

    upgraded flutter and packages

--- flutter/gymify/lib/screens/main_screens/membership_screen/khalti_payment_screen.dart
+++ flutter/gymify/lib/screens/main_screens/membership_screen/khalti_payment_screen.dart
@@ -21,89 +21,53 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'package:khalti'
 
+// import 'package:'
 
-import 'package:flutter/material.dart';
-import 'package:gymify/network/http.dart';
+// class KhaltiSDKDemo extends StatefulWidget {
+//   const KhaltiSDKDemo({super.key});
 
-class KhaltiPaymentScreen extends StatefulWidget {
-  final String publicKey;
-  final String pidx;
-  final String transactionId;
+//   @override
+//   State<KhaltiSDKDemo> createState() => _KhaltiSDKDemoState();
+// }
 
-  const KhaltiPaymentScreen({
-    Key? key,
-    required this.publicKey,
-    required this.pidx,
-    required this.transactionId,
-  }) : super(key: key);
+// class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
+//   late final Future<Khalti> khalti;
 
-  @override
-  State<KhaltiPaymentScreen> createState() => _KhaltiPaymentScreenState();
-}
+//   @override
+//   void initState() {
+//     super.initState();
+//     final payConfig = KhaltiPayConfig(
+//       publicKey: '__live_public_key__', // Merchant's public key
+//       pidx: pidx, // This should be generated via a server side POST request.
+//       environment: Environment.prod,
+//     );
 
-class _KhaltiPaymentScreenState extends State<KhaltiPaymentScreen> {
-  late final Future<Khalti> _khaltiFuture;
+//     khalti = Khalti.init(
+//       enableDebugging: true,
+//       payConfig: payConfig,
+//       onPaymentResult: (paymentResult, khalti) {
+//         log(paymentResult.toString());
+//       },
+//       onMessage: (
+//         khalti, {
+//         description,
+//         statusCode,
+//         event,
+//         needsPaymentConfirmation,
+//       }) async {
+//         log(
+//           'Description: $description, Status Code: $statusCode, Event: $event, NeedsPaymentConfirmation: $needsPaymentConfirmation',
+//         );
+//       },
+//       onReturn: () => log('Successfully redirected to return_url.'),
+//     );
+//   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    final payConfig = KhaltiPayConfig(
-      publicKey: widget.publicKey,
-      pidx: widget.pidx,
-      environment: Environment.prod, // Change to Environment.test for sandbox
-    );
-
-    _khaltiFuture = Khalti.init(
-      enableDebugging: true,
-      payConfig: payConfig,
-      onPaymentResult: (paymentResult, khalti) async {
-        print("Payment Result: ${paymentResult.payload}");
-        // After payment, call your backend to verify the payment status.
-        try {
-          final response = await httpClient.post('/verify-payment', data: {
-            'transaction_id': widget.transactionId,
-            'status': paymentResult.payload?.status ?? '',
-          });
-          // Handle response as needed (e.g. show confirmation, update UI)
-          Navigator.pop(context);
-        } catch (e) {
-          print("Error verifying payment: $e");
-          // Optionally show an error snackbar here.
-        }
-      },
-      onMessage: (khalti, {description, statusCode, event, needsPaymentConfirmation}) async {
-        print("Khalti Message: $description");
-        if (needsPaymentConfirmation == true) {
-          await khalti.verify();
-        }
-      },
-      onReturn: () {
-        print("User returned to app from Khalti Payment");
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Khalti>(
-      future: _khaltiFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            snapshot.data!.open(context);
-          });
-          return Scaffold(
-            appBar: AppBar(title: const Text("Khalti Payment")),
-            body: const Center(child: CircularProgressIndicator()),
-          );
-        }
-        return Scaffold(
-          appBar: AppBar(title: const Text("Initializing Payment")),
-          body: const Center(child: CircularProgressIndicator()),
-        );
-      },
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     // Rest of the code
+//   }
+// }
--- flutter/gymify/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/gymify/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,12 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import device_info_plus
+import file_picker
 import file_selector_macos
 import flutter_inappwebview_macos
 import flutter_local_notifications
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -17,9 +20,12 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
