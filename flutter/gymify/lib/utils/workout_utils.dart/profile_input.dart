29e52159c8c64fe53445ea2c252abad3af383777
AashishRauniyar <rauniyaaraashish@gmail.com>
2025-04-07 16:37:33 +0545

    minor bug fixed

---
+++ flutter/gymify/lib/utils/workout_utils.dart/profile_input.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class ProfileInput extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String? hintText;
+  final IconData? icon;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final Function(String)? onChanged;
+  final Function(String)? onFieldSubmitted;
+  final Widget? suffixIcon;
+  final bool readOnly;
+  final int maxLines;
+  final bool enabled;
+  final FocusNode? focusNode;
+  final bool autofocus;
+
+  const ProfileInput({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.hintText,
+    this.icon,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.suffixIcon,
+    this.readOnly = false,
+    this.maxLines = 1,
+    this.enabled = true,
+    this.focusNode,
+    this.autofocus = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: theme.colorScheme.primary.withOpacity(0.05),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextFormField(
+        controller: controller,
+        obscureText: isPassword,
+        keyboardType: keyboardType,
+        validator: validator,
+        onChanged: onChanged,
+        onFieldSubmitted: onFieldSubmitted,
+        readOnly: readOnly,
+        maxLines: maxLines,
+        enabled: enabled,
+        focusNode: focusNode,
+        autofocus: autofocus,
+        style: theme.textTheme.bodyLarge,
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          prefixIcon: icon != null ? Icon(icon, color: theme.colorScheme.primary) : null,
+          suffixIcon: suffixIcon,
+          filled: true,
+          fillColor: theme.colorScheme.surface,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: theme.colorScheme.outline,
+              width: 1.0,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: theme.colorScheme.outline.withOpacity(0.5),
+              width: 1.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: theme.colorScheme.primary,
+              width: 2.0,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: theme.colorScheme.error,
+              width: 1.0,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: theme.colorScheme.error,
+              width: 2.0,
+            ),
+          ),
+          labelStyle: TextStyle(
+            color: theme.colorScheme.onSurface.withOpacity(0.7),
+          ),
+          hintStyle: TextStyle(
+            color: theme.colorScheme.onSurface.withOpacity(0.5),
+          ),
+        ),
+      ),
+    );
+  }
+}
